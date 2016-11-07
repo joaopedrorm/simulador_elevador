2e114511a9e7e0d7bee0f4a4e8190141a0be7322
João Pedro <joaopedro6br@gmail.com>
2016-11-07 05:17:15 -0200

    definindo 3 simulações

--- src/main/java/jprm/simulador_elevadores/App.java
+++ src/main/java/jprm/simulador_elevadores/App.java
@@ -5,23 +5,76 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 /**
  * Hello world!
  *
  */
 public class App {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(App.class);
-	
+
 	public static void main(String[] args) {
 
-		Configuracao config = new Configuracao();
-		String conf = config.get("conf", "opcaoDefault");
-		logger.info(String.format("Configuração carregada: conf=%s", conf));
+		// Configuracao config = new Configuracao();
+		// String conf = config.get("conf", "opcaoDefault");
+		// logger.info(String.format("Configuração carregada: conf=%s", conf));
+
+		List<Pessoa> listaPessoas1 = (new PessoaLoader()).getListaPessoasResource();
+		List<Pessoa> listaPessoas2 = (new PessoaLoader()).getListaPessoasResource();
+		List<Pessoa> listaPessoas3 = (new PessoaLoader()).getListaPessoasResource();
+
+		// List<Pessoa> sublistaPessoas = listaPessoas.subList(0, 10);
+		//
+		// logger.info(String.format("listaPesoas=%s", sublistaPessoas));
+
+		/**
+		 * Simulação com controle sequencial
+		 */
+		ElevadorControle ctrlS = new ElevadorControleSequencial();
+		Simulador sim1 = new Simulador(ctrlS);
+		logger.info("Simulação com "+ sim1.getElevadorControle().getNome());
+		sim1.inicializar(listaPessoas1);
+
+		while (sim1.processarInstante() == SimulacaoStatus.PROCESSANDO) {
+			sim1.incrementarInstanteAtual();
+		}
+		logger.info("Simulação Finalizada");
 		
-		List<Pessoa> listaPessoas = (new PessoaLoader()).getListaPessoasResource();
+		/**
+		 * Simulação com controle Aleatório
+		 */
+		ElevadorControle ctrlA = new ElevadorControleAleatorio();
+		Simulador sim2 = new Simulador(ctrlA);
+		logger.info("Simulação com "+ sim2.getElevadorControle().getNome());
+		sim2.inicializar(listaPessoas2);
+
+		while (sim2.processarInstante() == SimulacaoStatus.PROCESSANDO) {
+			sim2.incrementarInstanteAtual();
+		}
+		logger.info("Simulação Finalizada");
 		
-		logger.info(String.format("listaPesoas=%s", listaPessoas));
+		/**
+		 * Simulação com controle menor tempo imediato
+		 */
+		ElevadorControle ctrlMTI = new ElevadorControleMenorTempoImediato();
+		Simulador sim3 = new Simulador(ctrlMTI);
+		logger.info("Simulação com "+ sim3.getElevadorControle().getNome());
+		sim3.inicializar(listaPessoas3);
+
+		while (sim3.processarInstante() == SimulacaoStatus.PROCESSANDO) {
+			sim3.incrementarInstanteAtual();
+		}
+		logger.info("Simulação Finalizada");
+		
+		/**
+		 * Imprime estatisticas
+		 */
+		sim1.imprimeEstatisticasSimulacao();
+		sim2.imprimeEstatisticasSimulacao();
+		sim3.imprimeEstatisticasSimulacao();
+
+		logger.info("Processo Finalizada");
+
 	}
+
 }
