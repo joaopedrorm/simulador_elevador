6dd71aa566945e56ce5cfff899a6017f4a5bf8f2
João Pedro <joaopedro6br@gmail.com>
2016-11-07 05:14:12 -0200

    alterando interface controle

--- src/main/java/jprm/simulador_elevadores/ElevadorControle.java
+++ src/main/java/jprm/simulador_elevadores/ElevadorControle.java
@@ -1,9 +1,12 @@
 package jprm.simulador_elevadores;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface ElevadorControle {
 	public void inicializar(List<Elevador> elevadores);
 
-	public Elevador decisao(Pessoa p);
+	public Elevador decisao(Pessoa p, LocalDateTime instanteAtual);
+	
+	public String getNome();
 }
